e7cc12007d13e88cfc901856bc650bf8c13c1978
Mo Henriquez <moses.henriquez@gmail.com>
2014-07-25 18:00:47 -0400

    styling and script updates

--- scripts/main.js
+++ scripts/main.js
@@ -33,41 +33,31 @@ requirejs.config({
             // If CDN fails, load from this location
             'lib/jquery-ui-1.10.3.min'
         ],
-        'modernizr': [
-            '//ajax.aspnetcdn.com/ajax/modernizr/modernizr-2.6.2',
-            // If CDN fails, load from this location
-            'lib/modernizr-2.8.0.min'
-        ],
-        'domReady': 'lib/domReady',
+
+        'modernizr': 'lib/modernizr-2.8.0.min',
+        'domReady': 'lib/domReady', // v2.0.1
 
         /**-----------------------------------
          jQuery Plugins
         -----------------------------------**/
 
-        'classie': 'lib/classie',
+        'classie': 'lib/classie', // v1.0.1
         'debounced': 'lib/jquery.debouncedresize',
-        'lazyloader': 'lib/jquery.bttrlazyloading.min',
-        'transitions': 'js/pagetransitions',
+        'lazyloader': 'lib/jquery.bttrlazyloading-1.0.5.min',
 
-        // offcanvas group
-        'offcanvas': 'lib/jquery.offcanvas',
-        'overthrow': 'lib/overthrow',
-        'hammer': 'lib/hammer',
-        'jhammer': 'lib/jquery.hammer',
-
-        // pagescroll group
+        // pagescroll
         'mousewheel': 'lib/jquery.mousewheel',
         'mwintent': 'lib/mwheelIntent',
         'jscroll': 'lib/jquery.jscrollpane.min',
 
-        // fullpage group
-        'easing': 'lib/jquery.easings-1.9.2.min',
+        // fullpage
+        'easings': 'lib/jquery.easings-1.9.2.min',
         'slimscroll': 'lib/jquery.slimscroll-1.3.2.min',
         'fullpage': 'lib/jquery.fullPage-2.0.7.min',
 
-        // sly group
-        
-        'sly': 'lib/sly.min'
+        // sly
+        'easing': 'lib/jquery.easing-1.3.min',
+        'sly': 'lib/sly-1.1.0.min'
 
         // -------------------------------------
     },
@@ -106,9 +96,13 @@ requirejs.config({
         {
             deps: ['jquery']
         },
-        'fullpage':
+        'easings':
         {
             deps: ['jquery']
+        },
+        'fullpage':
+        {
+            deps: ['easing', 'jquery']
         },
         'jscroll':
         {
@@ -134,10 +128,6 @@ requirejs.config({
         {
             deps: ['easing'],
             exports: 'sly'
-        },
-        'transitions':
-        {
-            deps: ['jquery']
         }
 
         // -------------------------------------------
@@ -164,7 +154,7 @@ require(['modernizr'], function ()
 
         domReady(function ()
         {
-            require(['angular', 'bootstrap', 'classie', 'easing', 'slimscroll', 'fullpage', 'lazyloader'], function (ng)
+            require(['angular', 'bootstrap', 'classie', 'easings', 'slimscroll', 'fullpage', 'lazyloader'], function (ng)
             {
                 // scripts for all mediums
                 
@@ -177,13 +167,19 @@ require(['modernizr'], function ()
                     'slidesNavigation': true,
                     'loopHorizontal': false,
                     'autoScrolling': false
-                    //'navigation': true, // if "autoScrolling" is false then "navigation" is false
-                    //'scrollOverflow': true // activate for mobile
 			    });
 
+                // Lazyload website images
+                $('.img-responsive').bttrlazyloading({
+                    //placeholder: 'data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==',
+                    backgroundcolor: 'transparent',
+                    animation: 'fadeIn',
+                    container: '.scroll-pane'
+                });
+
                 //YouTube Player Settings
-                var tag = document.createElement('script'); tag.src = "https://www.youtube.com/player_api"; var firstScriptTag = document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-                $('#playerA').click(function () { var player; player = new YT.Player('playerA', { height: '300', width: '320', videoId: 'sibcnHOLPec', playerVars: { 'autoplay': 1, 'playsinline': 1, 'showinfo': 0, 'controls': 1, 'enablejsapi': 1, 'modestbranding': 1, 'color': 'white', 'iv_load_policy': 3, 'rel': 0, 'theme': 'light'} }); });
+                //var tag = document.createElement('script'); tag.src = "https://www.youtube.com/player_api"; var firstScriptTag = document.getElementsByTagName('script')[0]; firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+                //$('#playerA').click(function () { var player; player = new YT.Player('playerA', { height: '300', width: '320', videoId: 'sibcnHOLPec', playerVars: { 'autoplay': 1, 'playsinline': 1, 'showinfo': 0, 'controls': 1, 'enablejsapi': 1, 'modestbranding': 1, 'color': 'white', 'iv_load_policy': 3, 'rel': 0, 'theme': 'light'} }); });
 
 
                 if (screenwidth < 1000) // load only tablet scripts
